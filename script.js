6a97bcf2448f4686647aece2293e112e5ade4914
Maryanne-Pensy <emiliapensy@gmail.com>
2025-10-22 17:17:49 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+    const hamburger = document.getElementById('hamburger');
+    const mobileMenu = document.getElementById('mobileMenu');
+
+    hamburger.addEventListener('click', () => {
+      mobileMenu.classList.toggle('show');
+    });
+
+    document.querySelectorAll('.mobile-menu a').forEach(link => {
+      link.addEventListener('click', () => {
+        mobileMenu.classList.remove('show');
+      });
+    });
+
+
